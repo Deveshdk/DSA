f878111e3d93dd0c287fce1e02c8ecc2e5611542
deveshkumar <devesh.dk@outlook.com>
2023-11-10 11:29:30 +0530

    Palindrome

---
+++ DSAProject/Easy/Palindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace DSAProject.Easy
+{
+	public class Palindrome
+	{
+		public bool IsPalindrome(int x)
+		{
+            int r = 0, c = x;
+            while (c > 0)
+            {
+                r = r * 10 + c % 10;
+                c /= 10;
+            }
+            return r == x;
+        }
+
+
+
+	}
+}
+
+//public class Solution
+//{
+//    public bool IsPalindrome(int x)
+//    {
+//        var y = x.ToString().ToCharArray();
+//        Array.Reverse(y); //Reverses char array.
+//        return x.ToString() == new string(y);
+//        //Checks if original string is equal to its reverse.
+//    }
+//}
+
