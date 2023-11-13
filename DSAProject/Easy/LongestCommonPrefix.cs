80edecf03863a76d835ef054f3b95526d5999986
deveshkumar <devesh.dk@outlook.com>
2023-11-14 00:47:39 +0530

    LongestCommonPrefix

---
+++ DSAProject/Easy/LongestCommonPrefix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace DSAProject.Easy
+{
+	public class LongestCommonPrefix
+	{
+		public string LongestCommonPrefixx(string[] str)
+		{
+			string output="";
+                for (int i = 0; i < str[0].Length; i++)
+                {
+                    foreach (string s in str)
+                    {
+                        if (s[i] == str[0][i])
+                        {
+
+                        }
+                    }
+                output += str[i][i];
+                }
+			return output;
+		}
+	}
+}
+
--- DSAProject/Program.cs
+++ DSAProject/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DSAProject.Easy;
+
 Console.WriteLine("Hello, World!");
 int num = 5 / 2;
 Console.WriteLine(num);
-
+string[] str = { "flower", "flow", "famous" };
+LongestCommonPrefix l = new LongestCommonPrefix();
+l.LongestCommonPrefixx(str);
